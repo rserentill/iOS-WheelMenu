abb8cd605d4063bab5c11b10876a67c1708ea72e
Roger Serentill Gené <hello@rogerserentill.com>
2017-12-01 15:44:52 +0100

    Initial Commit

---
+++ WheelMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WheelMenu
+//
+//  Created by Roger Serentill Gené on 1/12/17.
+//  Copyright © 2017 Roger Serentil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
